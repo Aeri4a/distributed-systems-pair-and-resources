44f6aa328cbf4de6bbcd0ae67eb1d7cb4bd82621
smazik02 <s.mazik2002@gmail.com>
2025-04-29 15:46:10 +0200

    switched to C++14, .clang_format

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
+
 #include <mpi.h>
 
-int main(int argc, char **argv)
-{
-    int size,rank, len;
+int main(int argc, char** argv) {
+    int size, rank, len;
     char processor[100];
-    MPI_Init(&argc,&argv);
+    MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
